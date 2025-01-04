b81ea120e2800fd9578b5f3af361442606f6fa6d
Buland-Choudhary <bulandchoudharysolalita@gmail.com>
2025-01-04 00:12:35 -0800

    Update .gitignore

---
+++ frontend/src/config.js
@@ -0,0 +1,4 @@
+// const apiUrl = "https://trackit-mm9q.onrender.com";  // URL of hosted backend
+const apiUrl = "http://localhost:5000";                 // URL of local backend
+
+export default apiUrl;
