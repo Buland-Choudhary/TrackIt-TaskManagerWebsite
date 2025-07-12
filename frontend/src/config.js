c46b0c100c4ac0fd670b1fc34827c3720a608a73
Buland Choudhary <bulandchoudharysolalita@gmail.com>
2025-07-12 12:46:00 +0530

    new url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://continued-ginni-bulandchoudhary-3b2bb51c.koyeb.app/";  // URL of hosted backend
+const apiUrl = "https://continued-ginni-bulandchoudhary-3b2bb51c.koyeb.app/project/trackit";  // URL of hosted backend
 //const apiUrl = "http://localhost:5000";                 // URL of local backend
 
 export default apiUrl;
