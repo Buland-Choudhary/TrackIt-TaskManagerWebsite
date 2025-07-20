385ad670a8e96c97485b63f4e993c9e4f6dfb31c
Buland Choudhary <bulandchoudharysolalita@gmail.com>
2025-07-20 17:36:28 +0530

    change BE host

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const apiUrl = "https://trackit-mm9q.onrender.com";  // URL of hosted backend
+const apiUrl = "https://portfolio-rkfu.onrender.com/project/trackit/";  // URL of hosted backend
 //const apiUrl = "http://localhost:5000";                 // URL of local backend
 
 export default apiUrl;
